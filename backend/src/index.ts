4f71bed34bf6a91f17376013820f00e7bfad54cd
Baron <bkkcreative23@gmail.com>
2023-12-28 15:28:51 -0600

    added register on the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 
+import userRoutes from "./routes/users";
+
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
@@ -10,8 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "hi" });
-});
+app.use("/api/users", userRoutes);
 
 app.listen(8080, () => console.log("listening on port 8080"));
